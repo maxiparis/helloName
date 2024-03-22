6bf4b91498cae835107004e05a0d98d939441a9f
“Max <mparis@hotmail.cl>
2024-03-22 00:42:18 -0600

    starter functionality (no keyboard yet)

--- helloName/ViewController.swift
+++ helloName/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var afterClickSaluteText: UILabel!
+    @IBOutlet weak var helloButton: UIButton!
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        instructionLabel.text = "Enter your name and select \"Hello!\""
+        afterClickSaluteText.isHidden = true
     }
-
-
+    
+    
+    @IBAction func helloButtonClicked(_ sender: UIButton) {
+        afterClickSaluteText.text = "Hello " + nameText.text! + "!"
+        afterClickSaluteText.isHidden = false
+        nameText.text = ""
+    }
 }
 

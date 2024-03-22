a39e13184fcc08510b58842ae4f53c799db1564e
“Max <mparis@hotmail.cl>
2024-03-22 00:13:16 -0600

    Initial Commit

---
+++ helloName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  helloName
+//
+//  Created by Maximiliano París Gaete on 3/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
